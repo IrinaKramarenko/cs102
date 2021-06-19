f134a524de589eacd8feae318b6f27e152c2a1d5
Irina Kramarenko <irinka.kramarenko288@gmail.com>
2021-06-19 20:31:56 +0300

    space

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -12,7 +12,7 @@ class NaiveBayesClassifier:
 
     def fit(self, X, y):
 
-        """ Fit Naive Bayes classifier according to X, y."""
+        """Fit Naive Bayes classifier according to X, y."""
         catalog = []
         for title, lable in zip(X, y):
             for word in title.split():
@@ -58,7 +58,7 @@ class NaiveBayesClassifier:
 
     def predict(self, X):
 
-        """ Perform classification on an array of test vectors X. """
+        """Perform classification on an array of test vectors X."""
         words = X.split()
         chance = []
         for cur_label in self.model["labels"]:
@@ -74,7 +74,7 @@ class NaiveBayesClassifier:
 
     def score(self, X_test, y_test):
 
-        """ Returns the mean accuracy on the given test data and labels. """
+        """Returns the mean accuracy on the given test data and labels."""
         correct = []
         for one in X_test:
             correct.append(self.predict(one))
